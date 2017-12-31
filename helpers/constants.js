88ef7a94024ec9e27eeaec795fa502ecb78f8e17
Bitrolium <34517861+bitrolium@users.noreply.github.com>
2017-12-31 12:19:19 +0330

    Initial commit

---
+++ helpers/constants.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+	activeDelegates: 51,
+	maximumVotes: 1,
+	addressLength: 208,
+	blockHeaderLength: 248,
+	confirmationLength: 77,
+	epochTime: new Date(Date.UTC(2017, 2, 21, 13, 0, 0, 0)),
+	fees:{
+		send: 10000,
+		vote: 100000000,
+		secondsignature: 10,
+		delegate: 5000000000,
+		multisignature: 10
+	},
+	feeStart: 1,
+	feeStartVolume: 10000 * 100000000,
+	fixedPoint : Math.pow(10, 8),
+	forgingTimeOut: 3060, // 102 blocks / 2 rounds
+	maxAddressesLength: 208 * 128,
+	maxAmount: 100000000,
+	maxClientConnections: 100,
+	maxConfirmations : 77 * 100,
+	maxPayloadLength: 2 * 1024 * 1024,
+	maxRequests: 10000 * 12,
+	maxSignaturesLength: 196 * 256,
+	maxTxsPerBlock: 50,
+	blocktime: 8,
+	numberLength: 100000000,
+	requestLength: 104,
+	rewards: {
+		milestones: [
+			100000000, // Initial Reward
+			100000000, // Milestone 1
+			100000000, // Milestone 2
+			100000000, // Milestone 3
+			100000000  // Milestone 4
+		],
+		offset: 756000,   // Start rewards at block, ie 70 days after net start
+		distance: 3000000, // Distance between each milestone
+	},
+	signatureLength: 196,
+	totalAmount: 7000000000000000,
+	unconfirmedTransactionTimeOut: 10800 // 1080 blocks
+};
